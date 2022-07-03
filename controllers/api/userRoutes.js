ff609b36700b981ac3479a0a0bc292bb6b29dd76
Javier R <javierresines@gmail.com>
2022-07-02 23:35:15 -0500

    working on login route for user

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -21,6 +21,43 @@ router.post('/', async (req, res) => {
       });
     } catch (err) {
       res.status(400).json(err);
-      
+
+    }
+  });
+
+  /////////////////////////////////////////
+
+  router.post('/login', async (req, res) => {
+    try {
+
+
+      const userData = await User.findOne({ where: { username: req.body.username }, });
+  
+      if (!userData) {
+        res
+          .status(400)
+          .json({ message: 'Incorrect email or password, please try again' });
+        return;
+      }
+  
+      const validPassword = userData.checkPassword(req.body.password);
+  
+      if (!validPassword) {
+        res
+          .status(400)
+          .json({ message: 'Incorrect email or password, please try again' });
+        return;
+      }//
+  
+      req.session.save(() => {
+        req.session.userId = userData.id;
+        req.session.username = userData.username
+        req.session.loggedIn = true;
+        
+        res.json({ user: userData, message: 'You are now logged in!' });
+      });
+  
+    } catch (err) {
+      res.status(400).json(err);
     }
   });
