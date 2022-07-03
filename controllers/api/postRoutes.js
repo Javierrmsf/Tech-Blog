78681a72ff17171416313dfcdfcc5f215f837e0b
Javier R <javierresines@gmail.com>
2022-07-02 22:36:11 -0500

    working on create route for posts

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,3 +1,26 @@
 const router = require('express').Router();
-const { Project } = require('../../models');
+const { Post} = require('../../models');
 const withAuth = require('../../utils/auth');
+
+
+
+
+
+
+router.post('/', withAuth, async (req, res) => {
+    
+    const body = req.body;
+
+    console.log(body)
+
+    try {
+      const newPost = await Post.create({
+        ...body,
+        user_id: req.session.user_id,
+      });
+  
+      res.status(200).json(newProject);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
