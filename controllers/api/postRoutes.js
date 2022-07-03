6e6ddde28ad458f9087079ef5a96598bbc7014f1
Javier R <javierresines@gmail.com>
2022-07-02 22:41:38 -0500

    working on delete route for posts

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -24,3 +24,28 @@ router.post('/', withAuth, async (req, res) => {
       res.status(400).json(err);
     }
   });
+
+
+  router.delete('/:id', withAuth, async (req, res) => {
+
+
+    try {
+      const [postdata1] = Post.destroy({
+        where: {
+
+          id: req.params.id,
+          
+        },
+      });
+  
+
+      if (!postdata1) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+  
+      res.status(200).json(postdata1);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
