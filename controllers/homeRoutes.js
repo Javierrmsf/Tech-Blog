a05265159ef6f2ebb72fed1f123bf759c6906c30
Javier R <javierresines@gmail.com>
2022-07-02 23:54:47 -0500

    making routes for login , signuppage and dashboard

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -44,3 +44,28 @@ router.get('/post/:id', withAuth, async (req, res) => {
     }
   });
 
+  router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/dashboard');
+      return;
+    }
+    res.render('login');
+  });
+
+
+
+
+  //////////////////////
+  
+  router.get('/signup', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/dashboard');
+
+      
+      return;
+    }
+  
+    res.render('signup');
+  });
+  
+  module.exports = router;
